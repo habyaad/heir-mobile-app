d61d8a16d3bbb5238b734371d1dfc377a046c2f0
habyaad <adeomotoshan@gmail.com>
2022-09-25 13:23:15 +0100

    implemented the general button widget

---
+++ lib/ui/widgets/general_button.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:heir/utils/app_text_styles.dart';
+
+import '../../heir_app_icons_icons.dart';
+import '../../utils/app_colors.dart';
+
+class GeneralButton extends StatelessWidget {
+  final String buttonText;
+  final bool hasShadow;
+  final Color buttonColor;
+  final Color textColor;
+  final Border? border;
+
+  const GeneralButton(
+      {Key? key,
+      required this.buttonText,
+      this.hasShadow = true,
+      this.buttonColor = AppColors.primaryColor1,
+      this.textColor = Colors.white,
+      this.border})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 175,
+      height: 60,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: buttonColor,
+          border: border?? border,
+          boxShadow: hasShadow
+              ? [
+                  BoxShadow(
+                      blurRadius: 20,
+                      offset: const Offset(0, 5),
+                      color: const Color(0xFF5887D1).withOpacity(0.25))
+                ]
+              : null),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            HeirAppIcons.fi_rr_cross_circle,
+            color: textColor,
+            size: 18,
+          ),
+          const SizedBox(
+            width: 9,
+          ),
+          Text(
+            buttonText,
+            style: AppTextStyles.title2Regular().copyWith(color: textColor),
+          )
+        ],
+      ),
+    );
+  }
+}
