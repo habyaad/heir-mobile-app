fd938dfab2f6bd1ac919f27baa593a973a85e6a4
habyaad <adeomotoshan@gmail.com>
2022-10-18 16:11:34 +0100

    model for the card screen

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -3,25 +3,46 @@ import 'package:money_formatter/money_formatter.dart';
 
 import '../heir_app_icons_icons.dart';
 
-enum TransactionType{credit, debit}
+enum TransactionType { credit, debit }
 
 class Transaction {
   final String name;
   final DateTime dateTime;
   final TransactionType transactionType;
   final double transactionAmount;
+  final List<Color> gradientColors;
+  final List<Color> solidColor;
 
+  List<String> months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ];
 
-  String get getTransactionAmount =>
-      MoneyFormatter(
+  String get getTransactionAmount => MoneyFormatter(
           amount: transactionAmount,
           settings: MoneyFormatterSettings(symbol: '€', fractionDigits: 0))
-          .output
-          .symbolOnLeft;
-  String get getMeridian => dateTime.hour < 12? 'am': 'pm';
+      .output
+      .symbolOnLeft;
 
-  Transaction({required this.name,
-    required this.dateTime,
-    required this.transactionType,
-    required this.transactionAmount});
+  String get getMeridian => dateTime.hour < 12 ? 'am' : 'pm';
+
+  String get getMonth => months[dateTime.month-1];
+
+  Transaction(
+      {required this.name,
+      required this.dateTime,
+      required this.transactionType,
+      required this.gradientColors,
+      required this.solidColor,
+      required this.transactionAmount});
 }
--- lib/ui/screens/cards/cards_model.dart
+++ lib/ui/screens/cards/cards_model.dart
@@ -1,19 +1,59 @@
+import 'dart:ui';
+
+import 'package:heir/utils/app_colors.dart';
 import 'package:stacked/stacked.dart';
 
 import '../../../heir_app_icons_icons.dart';
+import '../../../models/transaction.dart';
 import '../../widgets/action_button.dart';
 
 class CardScreenModel extends BaseViewModel {
   List buttons = [
+    const ActionButtons(icon: HeirAppIcons.paper_plane, actionText: 'Transfer'),
+    const ActionButtons(icon: HeirAppIcons.finances, actionText: 'Bills'),
     const ActionButtons(
-        icon: HeirAppIcons.paper_plane,
-        actionText: 'Transfer'),
-    const ActionButtons(
-        icon: HeirAppIcons.finances,
-        actionText: 'Bills'),
-    const ActionButtons(
-        icon: HeirAppIcons.iconoir_scan_qr_code,
-        actionText: 'QR Scan'),
+        icon: HeirAppIcons.iconoir_scan_qr_code, actionText: 'QR Scan'),
   ];
+  List<Transaction> transactions = [transaction1, transaction2, transaction3];
+  static Transaction transaction1 = Transaction(
+      name: 'Olawale Murphy',
+      transactionAmount: 76000,
+      dateTime: DateTime.utc(2022, 03, 23, 12, 23),
+      transactionType: TransactionType.debit,
+      gradientColors: [
+        const Color(0xffFBFCFC).withOpacity(1),
+        const Color(0xffFFFFFF).withOpacity(.1),
+      ],
+      solidColor: [
+        const Color(0xffD5C7FF),
+        const Color(0xffD5C7FF)
+      ]);
 
+  static Transaction transaction2 = Transaction(
+      name: 'Shopping',
+      transactionAmount: 82000,
+      dateTime: DateTime.utc(2022, 03, 22, 13, 41),
+      transactionType: TransactionType.debit,
+      gradientColors: [
+        const Color(0xffFBFCFC),
+        const Color(0xffFFFFFF).withOpacity(.1),
+      ],
+      solidColor: [
+        AppColors.secondaryColor1,
+        AppColors.secondaryColor1
+      ]);
+
+  static Transaction transaction3 = Transaction(
+      name: 'Kayode Bello',
+      transactionAmount: 2000,
+      dateTime: DateTime.utc(2022, 03, 21, 07, 35),
+      transactionType: TransactionType.debit,
+      gradientColors: [
+        const Color(0xffFBFCFC),
+        const Color(0xffFFFFFF).withOpacity(.1),
+      ],
+      solidColor: [
+        const Color(0xffFEF1EE),
+        const Color(0xffFEF1EE)
+      ]);
 }
